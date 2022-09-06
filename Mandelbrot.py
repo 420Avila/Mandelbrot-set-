75c95a2dc3bcce8260af232dfaca579016fb4b03
Oswaoldo Hernández Ávila <108857178+420Avila@users.noreply.github.com>
2022-09-05 23:24:32 -0500

    Update Mandelbrot.py

--- Mandelbrot.py
+++ Mandelbrot.py
@@ -1,6 +1,14 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
-
+print()
+print(" _  _ ___   ___              _ _       ")
+print("| || |__ \ / _ \            (_) |      ")
+print("| || |_ ) | | | | __ ___   ___| | __ _ ")
+print("|__   _/ /| | | |/ _` \ \ / / | |/ _` |")
+print("   | |/ /_| |_| | (_| |\ V /| | | (_| |")
+print("   |_|____|\___/ \__,_| \_/ |_|_|\__,_|")
+print()
+print() 
 def mandelbrot(re, im, max_iter):
     c = complex(re,im)
     z = 0.0j
